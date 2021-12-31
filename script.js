50dbf3132db70cb619e81520205d76bc27638f55
ThunderKlappe <timschley@gmail.com>
2021-12-30 21:09:45 -0800

    Clear Canvas button working

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const canvas = document.querySelector("#canvas");
 const rangeInput = document.querySelector('#myRange');
 const sliderValue = document.querySelector('#slider-value');
+const clearButton = document.querySelector("#clear-canvas-button");
 
 //determine size of grid
 let gridSize = rangeInput.value;
@@ -9,6 +10,8 @@ let gridSize = rangeInput.value;
 makeGrid(gridSize);
 sliderValue.textContent = `${rangeInput.value} x ${rangeInput.value}`;
 
+
+//change the size of the canvas based on the slider value
 rangeInput.oninput = function() {
     var value = (this.value-this.min)/(this.max-this.min)*100
     this.style.cssText = `background: linear-gradient(to right, #116466 0%, #116466  ${value}%, #fff ${value}%, white 100%)`
@@ -17,14 +20,19 @@ rangeInput.oninput = function() {
     makeGrid(gridSize);
 };
 
+//clear the canvas when the button is pressed
+clearButton.addEventListener("click", clearCanvas);
+
+
+
+
+//---------------Functions--------------------
 
 //this functions creates the grid based on the gridsize
 function makeGrid(gridSize) {
     let grid = [];
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.lastChild);
-    }
-    
+    deleteCanvas();
+
     for (let i = 1; i <= gridSize ** 2; i++) {
         //add elements to html
         grid[i] = document.createElement('div');
@@ -35,6 +43,19 @@ function makeGrid(gridSize) {
         grid[i].style.cssText = `height: ${(500 - gridSize * 2) / gridSize}px; width: ${(500 - gridSize * 2) / gridSize}px;`;
         //add eventListener to each grid piece to look for hover.
         grid[i].addEventListener('mouseover', changeColor);
+    }
+}
+
+//deletes all nodes and resets canvas at current grid size.
+function clearCanvas(e){
+    deleteCanvas();
+    makeGrid(gridSize);
+}
+
+//removes all nodes from canvas
+function deleteCanvas() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
     }
 }
 
